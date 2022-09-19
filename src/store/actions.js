74078600d76e6d0827de9d29218252f8e8eb4aa5
fred <ugodaycnzass1995@gmail.com>
2022-09-19 10:49:21 +0300

    fix

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,4 +1,5 @@
 export default {
+  // fetch запрос для получение валют
   async GET_CURRENCY ({ commit }) {
     const currneciesData = await fetch(
       'https://www.cbr-xml-daily.ru/daily_json.js'
